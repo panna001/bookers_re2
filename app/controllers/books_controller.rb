778c5430ce2f2cd827615e231ad3cb37e193888f
panna001 <sonar.pocket.0926@gmail.com>
2021-01-20 14:53:53 +0000

    [Add]createメソッド

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,22 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book)
+  end
+
   def show
   end
 
   def edit
+
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
