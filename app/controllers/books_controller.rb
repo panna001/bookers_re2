fb6630c80c23c02370f0ece686334e161b5f595e
panna001 <sonar.pocket.0926@gmail.com>
2021-01-20 16:18:04 +0000

    [Add]render

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,12 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book), notice: "Book was successfully created."
+    if @book.save
+      redirect_to book_path(@book), notice: "Book was successfully created."
+    else
+      @books = Book.all
+      render "index"
+    end
   end
 
   def show
@@ -20,8 +24,11 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book), notice: "Book was successfully created."
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "Book was successfully created."
+    else
+      render "edit"
+    end
   end
 
   private
